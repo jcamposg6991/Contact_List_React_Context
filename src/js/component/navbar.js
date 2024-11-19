70d1d2db6b3ebf8c187aa2ef54ed74ffb5a30ac8
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2024-11-19 03:24:56 +0000

    addContact.jsx coding

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3 mx-2">
+		<nav className="navbar navbar-light bg-light mb-3 px-3">
 			<h1>CONTACTS</h1>
 			<div className="ml-auto">
 				<Link to="/demo">
