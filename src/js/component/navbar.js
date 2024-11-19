160397c07594e37a4328234dc7d38b5144c1cf62
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2024-11-19 03:29:19 +0000

    route to addContact in navbar coding

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,7 +6,7 @@ export const Navbar = () => {
 		<nav className="navbar navbar-light bg-light mb-3 px-3">
 			<h1>CONTACTS</h1>
 			<div className="ml-auto">
-				<Link to="/demo">
+				<Link to="/addContact">
 					<button className="btn btn-success">Add new contact</button>
 				</Link>
 			</div>
